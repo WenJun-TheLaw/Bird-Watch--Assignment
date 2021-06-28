fa91efc5f7d0996d08d683c58b55f04c083813f0
InstaKarma4 <lawwenjun@gmail.com>
2021-06-28 11:07:03 +0800

    Camera Functionality implemented
    User can now open the camera to take a picture of the bird, but app camera permissions are a bit funky right now. Only works once then the camera breaks.

--- app/src/main/java/android/jun/birdwatch/BirdFragment.java
+++ app/src/main/java/android/jun/birdwatch/BirdFragment.java
@@ -1,10 +1,16 @@
 package android.jun.birdwatch;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -18,13 +24,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -32,6 +45,7 @@ public class BirdFragment extends Fragment {
     private static final String ARG_BIRD_ID  = "bird_id";
     private static final String DIALOG_DATE  = "DialogDate";
     private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_PHOTO = 1;
     private Bird mBird;
     private EditText mNameField;
     private EditText mDescriptionField;
@@ -57,6 +71,7 @@ public class BirdFragment extends Fragment {
         BirdList.get(requireActivity()).updateBird(mBird);
     }
 
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode != Activity.RESULT_OK){
@@ -66,6 +81,11 @@ public class BirdFragment extends Fragment {
             Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mBird.setDate(date);
             updateDate();
+        }
+        else if (requestCode == REQUEST_PHOTO){
+            Uri uri = FileProvider.getUriForFile(requireActivity(), "android.jun.birdwatch.fileprovider", mPhotoFile);
+            requireActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            updatePhotoView();
         }
     }
 
@@ -155,7 +175,35 @@ public class BirdFragment extends Fragment {
 
         //Setting up take photo button and photo ImageView
         mPhotoButton = (ImageButton) v.findViewById(R.id.bird_camera);
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //Determining if device supports camera function
+        boolean canTakePhoto = (mPhotoFile != null) && (captureImage.resolveActivity(requireActivity().getPackageManager()) != null);
+        mPhotoButton.setOnClickListener(view -> {
+            //Device supports camera function
+            if(canTakePhoto){
+                //Check whether app has permission to use camera
+                boolean hasPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+                if(hasPermission){
+                    //App has permission
+                    startCameraIntent(captureImage);
+                }
+                else{
+                    //App does not have permission, ask for it
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+                }
+            }
+            //Device does not support camera function
+            else{
+                Toast toast = Toast.makeText(getContext(), getString(R.string.device_no_support_camera), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        //Setting up photo view and update it
         mPhotoView = (ImageView) v.findViewById(R.id.bird_photo);
+        updatePhotoView();
+
         return v;
     }
 
@@ -182,10 +230,6 @@ public class BirdFragment extends Fragment {
         }
     }
 
-    private void updateDate() {
-        mDateButton.setText(mBird.getDate().toString());
-    }
-
     public static BirdFragment newInstance(UUID birdId) {
         Bundle args = new Bundle();
         args.putSerializable(ARG_BIRD_ID, birdId);
@@ -201,5 +245,44 @@ public class BirdFragment extends Fragment {
         String sighting = getString(R.string.bird_sighting, mBird.getName(), dateString, mBird.getDescription());
         return sighting;
     }
-}
 
+    private void updateDate() {
+        mDateButton.setText(mBird.getDate().toString());
+    }
+
+    private void updatePhotoView(){
+        //If no photo, display nothing
+        if(mPhotoFile == null || !mPhotoFile.exists()){
+            mPhotoView.setImageDrawable(null);
+        }
+        //IF got, scale the bitmap and dispay it
+        else{
+            Bitmap bitmap = PictureUtils.getScaledBitmap(mPhotoFile.getPath(), requireActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startCameraIntent(captureImage);
+                } else {
+                    //User denied permission, show sad toast
+                    Toast toast = Toast.makeText(getContext() ,"Permission denied by user :(", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+
+
+    private void startCameraIntent(Intent captureImage) {
+        Uri uri = FileProvider.getUriForFile(requireActivity(), "android.jun.birdwatch.fileprovider", mPhotoFile);
+        captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+        List<ResolveInfo> cameraActivities  = requireActivity().getPackageManager().queryIntentActivities(captureImage, PackageManager.MATCH_DEFAULT_ONLY);
+        for(ResolveInfo activity : cameraActivities){
+            getActivity().grantUriPermission(activity.activityInfo.packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+
+        startActivityForResult(captureImage, REQUEST_PHOTO);
+    }
